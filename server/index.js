ddc0f1d71807e04ca428f697f0e38bc76a77df04
Abhishek Saraswat <saraswatbhi0007@gmail.com>
2024-02-01 23:07:56 +0530

    code

--- server/index.js
+++ server/index.js
@@ -21,7 +21,12 @@ mongoose
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
+app.use(cors({
+  origin:[""],
+  methods:["POST","GET"],
+  credentials:true
+ 
+} ));
 const server = app.listen(process.env.PORT, () =>
   console.log(`Server started on ${process.env.PORT}`)
 );
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -13,5 +13,4 @@ router.post("/register", register);
 router.get("/allusers/:id", getAllUsers);
 router.post("/setavatar/:id", setAvatar);
 router.get("/logout/:id", logOut);
-
 module.exports = router;
